701aec41b51fe93a12020af39ceed73b1530133b
yKicchan <ykicchanapp@gmail.com>
2020-07-04 00:25:50 +0900

    feat: ヘッダーにリンクと外部リンクのアイコン追加

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,32 @@
 import React from 'react'
 import styles from './styles.module.scss'
 import Link from 'next/link'
+import Icon from '../Icon'
 
 const Header: React.FC = () => {
   return (
     <>
-      <header className={styles.container}>
-        <Link href="/">
-          <a>Home</a>
-        </Link>
-        <div>This is Header</div>
+      <header className={styles.component}>
+        <nav>
+          <ul className={styles.links}>
+            <li>
+              <Link href="/">
+                <a>Top</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/about">
+                <a>About</a>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <div className={styles.icons}>
+          <Icon icon={['fab', 'twitter']} href="https://twitter.com/yKicchan" />
+          <Icon icon={['fab', 'github']} href="https://github.com/yKicchan" />
+        </div>
       </header>
+      <hr className={styles.hr} />
     </>
   )
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,12 @@ import { AppProps } from 'next/app'
 import React from 'react'
 import '~/styles/index.css'
 
+import { config, library } from '@fortawesome/fontawesome-svg-core'
+config.autoAddCss = false
+
+import { faTwitter, faGithub } from '@fortawesome/free-brands-svg-icons'
+library.add(faTwitter, faGithub)
+
 const App = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />
 
 export default App
