ce6d3cf2be86424063eeebe2b0970ed372dc7cdf
yKicchan <ykicchanapp@gmail.com>
2020-07-04 00:32:26 +0900

    feat: copy right

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from './styles.module.scss'
+import Icon from '../Icon'
+
+const Footer: React.FC = () => {
+  return (
+    <footer className={styles.component}>
+      <small className={styles.copy}>
+        &copy; 2020 yKicchan All Rights Reserved.
+      </small>
+    </footer>
+  )
+}
+
+export default Footer
--- src/layouts/Default/index.tsx
+++ src/layouts/Default/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Head from 'next/head'
 import Header from '~/components/Header'
 import styles from './styles.module.scss'
+import Footer from '~/components/Footer'
 
 const Layout: React.FC = (props) => (
   <>
@@ -10,6 +11,7 @@ const Layout: React.FC = (props) => (
     </Head>
     <Header />
     <main className={styles.content}>{props.children}</main>
+    <Footer />
   </>
 )
 
