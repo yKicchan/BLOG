3ea32e328e12d8cf486d2cb00d086b43f899535d
yKicchan <ykicchanapp@gmail.com>
2020-07-03 20:20:42 +0900

    feat: 記事一覧の表示

--- src/@types/mdx.d.ts
+++ src/@types/mdx.d.ts
@@ -45,6 +45,7 @@ declare module '*.mdx' {
     excerpt: string
     createdAt: string
     updatedAt?: string
+    path: string
   }
 
   export interface Post {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,19 +2,45 @@ import { NextPage } from 'next'
 import Link from 'next/link'
 import React from 'react'
 import Layout from '~/layouts/Default'
+import fs from 'fs'
+import { Metadata } from '*.mdx'
 
-const Page: NextPage = () => (
-  <Layout>
-    <h1>Hello world!!</h1>
-    <ul>
-      <li>
-        <Link href="/about">About</Link>
-      </li>
-      <li>
-        <Link href="/posts/test">Test</Link>
-      </li>
-    </ul>
-  </Layout>
-)
+const Page: NextPage<{ metadataList: Metadata[] }> = ({ metadataList }) => {
+  return (
+    <Layout>
+      <h1>Hello world!!</h1>
+      <ul>
+        <li>
+          <Link href="/about">
+            <a>About</a>
+          </Link>
+        </li>
+        {metadataList.map((meta) => (
+          <li key={meta.path}>
+            <Link href={meta.path}>
+              <a>{meta.title}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </Layout>
+  )
+}
+
+export async function getStaticProps() {
+  const dir = '/src/pages/posts/'
+  const files = fs.readdirSync(process.cwd() + dir)
+  const mdxDocs = Promise.all(files.map((file) => import('./posts/' + file)))
+  const metadataList = (await mdxDocs).map((mdx, i) => ({
+    ...mdx.metadata,
+    path: `/posts/${files[i].substring(0, files[i].indexOf('.'))}`,
+  }))
+
+  return {
+    props: {
+      metadataList,
+    },
+  }
+}
 
 export default Page
