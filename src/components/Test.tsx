948f262d67ed618a17b3b398d82cda1618acc571
yKicchan <ykicchanapp@gmail.com>
2020-07-03 18:35:15 +0900

    delete: 不要なもの削除

--- src/components/Test.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-const Test: React.FC = () => <div>Test</div>
-
-export default Test
--- tailwind.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  purge: ["./src/components/**/*.tsx", "./src/pages/**/*.tsx"],
-  theme: {},
-  variants: {},
-  plugins: [],
-};
