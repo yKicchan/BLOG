7c102261bc4da42d929591fb1aa9293cb47c3cbd
yKicchan <ykicchanapp@gmail.com>
2020-07-04 16:02:34 +0900

    fix: ヘッダーとフッターのデザイン調整

--- src/components/organisms/Header/index.tsx
+++ src/components/organisms/Header/index.tsx
@@ -5,29 +5,26 @@ import Icon from '../../atoms/Icon'
 
 const Header: React.FC = () => {
   return (
-    <>
-      <header className={styles.component}>
-        <nav>
-          <ul className={styles.links}>
-            <li>
-              <Link href="/">
-                <a>Top</a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/about">
-                <a>About</a>
-              </Link>
-            </li>
-          </ul>
-        </nav>
-        <div className={styles.icons}>
-          <Icon icon={['fab', 'twitter']} href="https://twitter.com/yKicchan" />
-          <Icon icon={['fab', 'github']} href="https://github.com/yKicchan" />
-        </div>
-      </header>
-      <hr className={styles.hr} />
-    </>
+    <header className={styles.component}>
+      <nav>
+        <ul className={styles.links}>
+          <li>
+            <Link href="/">
+              <a>Top</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/about">
+              <a>About</a>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <div className={styles.icons}>
+        <Icon icon={['fab', 'twitter']} href="https://twitter.com/yKicchan" />
+        <Icon icon={['fab', 'github']} href="https://github.com/yKicchan" />
+      </div>
+    </header>
   )
 }
 
