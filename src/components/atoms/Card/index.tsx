26cd9242e776632d9e577453e554c78daf7de5a7
Yukiya Murata <yKicchanApp@gmail.com>
2020-07-07 22:14:13 +0900

    update: About スキルカードに裏情報を仕込む遊び (#18)

--- src/components/atoms/Card/index.tsx
+++ src/components/atoms/Card/index.tsx
@@ -2,15 +2,18 @@ import React from 'react'
 import styles from './styles.module.scss'
 
 interface P {
+  onClick?: () => void
   className?: string
   tag?: 'dl' | 'header' | 'article'
 }
 
-const Card: React.FC<P> = ({ children, className, tag }) => {
+const Card: React.FC<P> = ({ children, onClick, className, tag }) => {
   const style = className || ''
   const Component = tag || 'div'
   return (
-    <Component className={`${styles.component} ${style}`}>{children}</Component>
+    <Component onClick={onClick} className={`${styles.component} ${style}`}>
+      {children}
+    </Component>
   )
 }
 
