9ab13fa85fda70f954dd480bbb01e4165bbb5206
yKicchan <ykicchanapp@gmail.com>
2021-04-02 10:36:51 +0900

    add: fortawesome fas package

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,9 +6,10 @@ import Layout from '~/components/layouts/Default'
 import { config, library } from '@fortawesome/fontawesome-svg-core'
 config.autoAddCss = false
 
-import { faTwitter, faGithub } from '@fortawesome/free-brands-svg-icons'
-import { faClock } from '@fortawesome/free-regular-svg-icons'
-library.add(faTwitter, faGithub, faClock)
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+library.add(fab, far, fas)
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
