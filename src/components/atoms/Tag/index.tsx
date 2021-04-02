ab43e2400d3c8341023cc60f44d51cdc5ded146c
yKicchan <ykicchanapp@gmail.com>
2021-04-02 14:37:41 +0900

    fix: Tag 型エラー

--- src/components/atoms/Tag/index.tsx
+++ src/components/atoms/Tag/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import styles from './styles.module.scss'
 
 interface P {
-  className: string
+  className?: string
 }
 
 const Tag: React.FC<P> = ({ children, className }) => {
