25fe351a3020fa24275a9938dfd45c038c345c13
yKicchan <ykicchanapp@gmail.com>
2021-04-02 10:37:08 +0900

    update: Tag # つける

--- src/components/atoms/Tag/index.tsx
+++ src/components/atoms/Tag/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import styles from './styles.module.scss'
 
 const Tag: React.FC = ({ children }) => {
-  return <span className={styles.component}>{children}</span>
+  return <span className={styles.component}># {children}</span>
 }
 
 export default Tag
