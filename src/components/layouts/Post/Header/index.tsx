33345b3b3d1b40bc24373da052820b01dbcfee99
yKicchan <ykicchanapp@gmail.com>
2021-04-04 23:28:25 +0900

    update: 記事ヘッダー デザインの更新と、スクロール時に上部固定表示
    上部固定で使えるTOC はまだ

--- src/components/layouts/Post/Header/index.tsx
+++ src/components/layouts/Post/Header/index.tsx
@@ -1,35 +1,53 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import styles from './styles.module.scss'
 import Card from '~/components/atoms/Card'
 import Tag from '~/components/atoms/Tag'
+import { useIntersection } from '~/hooks/useIntersection'
+import { FontAwesomeIcon, FontAwesomeIconProps } from '@fortawesome/react-fontawesome'
 
 interface P {
   meta: Meta
 }
 
 const Header: React.FC<P> = ({ meta }) => {
+  const ref = useRef<HTMLHeadingElement>(null)
+  const intersection = useIntersection(ref)
+  const time = meta.updatedAt || meta.createdAt
+  const timeIcon: FontAwesomeIconProps['icon'] = meta.updatedAt ? ['fas', 'sync-alt'] : ['far', 'clock']
+  const timeTitle = meta.updatedAt ? `公開: ${meta.createdAt}\n更新: ${meta.updatedAt}` : ''
+
   return (
-    <Card tag="header" className={styles.component}>
-      <h1>{meta.title}</h1>
-      <p>
-        <time className={styles.time} dateTime={meta.createdAt}>
-          {meta.createdAt}
-        </time>
-        {meta.updatedAt && (
-          <time
-            className={`${styles.time} ${styles.note}`}
-            dateTime={meta.updatedAt}
-          >
-            最終更新日: {meta.updatedAt}
+    <>
+      <Card tag="header" className={styles.component}>
+       <h1 className={styles.title}>{meta.title}</h1>
+        <p>
+          <time className={styles.time} dateTime={time} title={timeTitle}>
+            <FontAwesomeIcon icon={timeIcon} className={styles.timeIcon} />
+            {time}
           </time>
-        )}
-      </p>
-      <div className={styles.tags}>
-        {meta.tags.map((tag) => (
-          <Tag key={tag}>{tag}</Tag>
-        ))}
+        </p>
+        <div className={styles.tags}>
+          {meta.tags.map((tag) => (
+            <Tag key={tag}>{tag}</Tag>
+          ))}
+        </div>
+      </Card>
+      <div ref={ref}>
+        {/* スクロール固定ヘッダー表示判定用のダミー要素 */}
       </div>
-    </Card>
+      {!intersection &&
+        <header className={styles.floatHeaderContainer}>
+          <div className={styles.floatHeader}>
+            <div className={styles.floatTitle} title={meta.title}>
+              {meta.title}
+            </div>
+            <button className={styles.floatButton}>
+              <FontAwesomeIcon icon={['fas', 'angle-down']} />
+            </button>
+          </div>
+        </header>
+      }
+    </>
   )
 }
 
